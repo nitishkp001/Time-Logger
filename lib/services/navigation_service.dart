b2f9523e889d4d494915177bf5f0f4bd3d287ffc
Nitish Prajapati <nitish.work001@gmail.com>
2025-01-12 20:04:29 +0530

    chore: Test commit

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+/**
+ * Navigation Service
+ */
 class NavigationService {
-  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
 }
